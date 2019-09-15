af49025f6c736d82ca339f38ce9422fd7d2ae914
Björn Petersen <pheasn@gmail.com>
2019-09-15 17:29:09 +0200

    Configure cmake build for linux

---
+++ native/volctl-alsa.cpp
@@ -0,0 +1,12 @@
+#include "volctl.h"
+
+JNIEXPORT jint JNICALL Java_net_bjoernpetersen_volctl_VolumeControl_getVolumeNative
+        (JNIEnv *, jobject) {
+    // FIXME implement
+    return 0;
+}
+
+JNIEXPORT void JNICALL Java_net_bjoernpetersen_volctl_VolumeControl_setVolumeNative
+        (JNIEnv *, jobject, jint value) {
+    // FIXME implement
+}
